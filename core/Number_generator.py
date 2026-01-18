106f6c07dc38240baa9247b42ffb9d73414bd31c
reetolop <wintereetolop0909@gmail.com>
2026-01-18 23:56:38 +0900

    feat(core): add random integer generator class main module

---
+++ core/Number_generator.py
@@ -0,0 +1,21 @@
+import random
+
+
+class randomize_int:
+
+    def __init__(self, min_val, max_val):
+        self.min_val = min_val
+        self.max_val = max_val
+        
+        self.chosen = random.randint(self.min_val, self.max_val)
+    
+
+    
+    @property
+    def rolled(self):
+        answer = random.randint(self.min_val, self.max_val)
+        return answer
+
+
+
+    
